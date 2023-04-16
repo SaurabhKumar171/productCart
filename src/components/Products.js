f96b374be04fd78dde6de645a4bd0838f0d5bfed
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2023-04-16 13:12:35 +0530

    first commit

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
 
 import './Products.css'
 
 import Itemdate  from './Itemdate';
 
 
+
 function Products(props){
+
+        const [title , setTitle] =useState(props.title);
+       function clickHandler(){
+          if(title ==='Popcorn'){
+                setTitle('Maggie');
+          }
+          else{
+                setTitle('Popcorn');
+          }
+        
+        console.log('I am clicked');
+       }
+
     return <div className='product'>
             <Itemdate date={props.date}/> 
 
             <div className='product-title'>
-                 <h1>{props.title}</h1>
+                 <h1>{title}</h1>
             </div>
 
-            butt
+            <button onClick={clickHandler}>Add to cart</button>
     </div>;
 }
 
